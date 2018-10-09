5503d3e1790006ad1b7e5e0263fc4defad917a46
Andreas Pichler <andreaspichler@tsn.at>
2018-10-09 17:24:16 +0200

    add mvn carConfigApp

---
+++ my-app/src/main/java/org/pichlera/carconfig/App.java
@@ -0,0 +1,13 @@
+package org.pichlera.carconfig;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
